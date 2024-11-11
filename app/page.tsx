31cf3e352af0c99ef3acc961b631289ec2de54e8
antoine-klz <antoine.klz@proton.me>
2024-11-11 20:18:16 +0100

    Created first version of services section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/navbar";
 import { BlockPage } from "@/components/page";
+import Services from "@/components/services";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <BlockPage />
+      <Services />
     </>
   );
 }
--- components/page.tsx
+++ components/page.tsx
@@ -8,8 +8,8 @@ import Link from "next/link";
 export function BlockPage() {
   return (
     <div className="overflow-x-hidden">
-      <div className="min-h-screen bg-[#144723] pt-20 relative before:absolute before:content-[''] before:w-full before:h-screen before:top-0 before:right-0 before:bg-gradient-to-br before:from-[#144723] before:via-[#1a5f2e] before:to-[#8AEA7C] before:opacity-30 before:blur-[80px]">
-        <div className="w-full max-w-5xl mx-auto px-6">
+      <div className="min-h-screen bg-[#144723] pt-20 relative before:absolute before:content-[''] before:w-full before:h-screen before:top-0 before:left-0 before:bg-gradient-to-br before:from-[#13d24c] before:via-[#0b3617] before:to-[#8AEA7C] before:opacity-30 before:blur-[80px]">
+        <div className="w-full max-w-5xl mx-auto px-6 lg:px-0">
           <div className="mt-8 lg:mt-20 flex flex-col lg:flex-row h-full lg:justify-between">
             <div className="flex flex-col lg:w-1/2 justify-between mb-8 lg:mb-0">
               <p className="text-[#8AEA7C] mb-8 lg:mb-10">WILKOMMEN</p>
@@ -35,7 +35,7 @@ export function BlockPage() {
                   className="rounded-full shadow-lg object-cover"
                 />
               </div>
-              <div className="absolute -bottom-4 left-28 -translate-x-1/2 lg:left-240 lg:translate-x-0 w-40 h-40 bg-[#fdff45] rounded-full flex items-center justify-center">
+              <div className="absolute -bottom-4 md:left-0 left-20 -translate-x-1/2 lg:left-240 lg:translate-x-0 w-40 h-40 bg-[#fdff45] rounded-full flex items-center justify-center">
                 <div className="flex flex-col items-center space-y-2">
                   <ArrowUpRight size={24} color="#144723" />
                   <div className="text-black font-semibold hover:underline">
